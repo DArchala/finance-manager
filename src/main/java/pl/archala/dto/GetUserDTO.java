abd51fa2e3bd1f9c6220f89bd47148ea86e6ffbe
darch <damianarchala@gmail.com>
2024-03-26 16:00:37 +0100

    Implement GetUserDTO

--- src/main/java/pl/archala/dto/GetUserDTO.java
+++ src/main/java/pl/archala/dto/GetUserDTO.java
@@ -1,4 +1,8 @@
 package pl.archala.dto;
 
-public record GetUserDTO() {
+import pl.archala.enums.NotificationChannel;
+
+public record GetUserDTO(String username, char[] password, String phoneNumber, String email,
+                         NotificationChannel notificationChannel) {
+
 }
