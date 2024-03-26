74f800b8147b9f6dae90f35bcdca2d09d3764bf5
darch <damianarchala@gmail.com>
2024-03-26 15:58:32 +0100

    Set AddUserDTO validation

--- src/main/java/pl/archala/dto/AddUserDTO.java
+++ src/main/java/pl/archala/dto/AddUserDTO.java
@@ -1,4 +1,22 @@
 package pl.archala.dto;
 
-public record AddUserDTO() {
+import jakarta.validation.constraints.*;
+import pl.archala.enums.NotificationChannel;
+
+public record AddUserDTO(@NotBlank(message = "Username must not be blank")
+                         @Size(min = 6, max = 30, message = "Username must contains min 6 and max 30 chars")
+                         String username,
+
+                         @NotBlank(message = "User password must not be blank")
+                         @Size(min = 8, max = 20, message = "User password must contains min 8 and max 20 chars")
+                         String password,
+
+                         @NotBlank(message = "User phone number must not be blank")
+                         @Pattern(regexp = "^\\d{9}$")
+                         String phoneNumber,
+
+                         @NotBlank(message = "User e-mail must not be blank") @Email String email,
+
+                         @NotNull(message = "User notification channel must not be null") NotificationChannel notificationChannel) {
+
 }
