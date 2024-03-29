dffd4be2748a6241ce10545776d539da864fb7dc
darch <damianarchala@gmail.com>
2024-03-29 11:55:48 +0100

    Refactoring in BalanceIdentifierGenerator

--- src/main/java/pl/archala/generator/BalanceIdentifierGenerator.java
+++ src/main/java/pl/archala/generator/BalanceIdentifierGenerator.java
@@ -10,18 +10,21 @@ import java.security.SecureRandom;
 @Component
 public class BalanceIdentifierGenerator implements IdentifierGenerator {
 
-    private final int balanceIdLength;
+    private final int length;
+    private final SecureRandom random;
+    private final String characters;
 
-    public BalanceIdentifierGenerator(@Value("${balance-id-length}") int balanceIdLength) {
-        this.balanceIdLength = balanceIdLength;
+    public BalanceIdentifierGenerator(@Value("${balance.id.length}") int length,
+                                      @Value("${balance.id.characters}") String characters) {
+        this.length = length;
+        this.random = new SecureRandom();
+        this.characters = characters;
     }
 
     @Override
     public Object generate(SharedSessionContractImplementor session, Object object) {
-        String characters = "0123456789";
-        SecureRandom random = new SecureRandom();
-        StringBuilder sb = new StringBuilder(balanceIdLength);
-        for (int i = 0; i < balanceIdLength; i++) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
             sb.append(characters.charAt(random.nextInt(characters.length())));
         }
         return sb.toString();
