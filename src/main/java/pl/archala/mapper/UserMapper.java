b8ddc65e6674ec769736c3394c31cab406fc996e
darch <damianarchala@gmail.com>
2024-03-31 01:13:10 +0100

    Move user password encoding to users mapper

--- src/main/java/pl/archala/mapper/UserMapper.java
+++ src/main/java/pl/archala/mapper/UserMapper.java
@@ -1,5 +1,7 @@
 package pl.archala.mapper;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import pl.archala.dto.user.AddUserDTO;
 import pl.archala.dto.user.GetUserDTO;
@@ -7,20 +9,23 @@ import pl.archala.dto.user.UserNotificationData;
 import pl.archala.entity.User;
 
 @Component
+@RequiredArgsConstructor
 public class UserMapper {
 
-    public GetUserDTO toGetDto(User user) {
-        return new GetUserDTO(user.getId(), user.getUsername(), user.getPhone(), user.getEmail(), user.getNotificationChannel());
-    }
+    private final PasswordEncoder passwordEncoder;
 
     public User toEntity(AddUserDTO addUserDTO) {
         User user = new User();
         user.setUsername(addUserDTO.username());
-        user.setPassword(addUserDTO.password().toCharArray());
+        user.setPassword(passwordEncoder.encode(addUserDTO.password()).toCharArray());
         user.setPhone(addUserDTO.phone());
         user.setEmail(addUserDTO.email());
         user.setNotificationChannel(addUserDTO.notificationChannel());
         return user;
+    }
+
+    public GetUserDTO toGetDto(User user) {
+        return new GetUserDTO(user.getId(), user.getUsername(), user.getPhone(), user.getEmail(), user.getNotificationChannel());
     }
 
     public UserNotificationData toUserNotificationDTO(User user) {
--- src/main/java/pl/archala/service/users/UsersServiceImpl.java
+++ src/main/java/pl/archala/service/users/UsersServiceImpl.java
@@ -2,13 +2,11 @@ package pl.archala.service.users;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.archala.dto.user.AddUserDTO;
 import pl.archala.dto.user.GetUserDTO;
 import pl.archala.dto.user.UserNotificationData;
-import pl.archala.entity.User;
 import pl.archala.exception.UsersConflictException;
 import pl.archala.mapper.UserMapper;
 import pl.archala.repository.UsersRepository;
@@ -23,15 +21,11 @@ public class UsersServiceImpl implements UsersService {
     private final UsersRepository usersRepository;
     private final UsersValidator usersValidator;
     private final UserMapper userMapper;
-    private final PasswordEncoder passwordEncoder;
 
     @Override
     public GetUserDTO registerUser(AddUserDTO addUserDTO) throws UsersConflictException {
         usersValidator.validateUsersConflicts(addUserDTO);
-        User userToSave = userMapper.toEntity(addUserDTO);
-        userToSave.setPassword(passwordEncoder.encode(addUserDTO.password()).toCharArray());
-        User savedUser = usersRepository.save(userToSave);
-        return userMapper.toGetDto(savedUser);
+        return userMapper.toGetDto(usersRepository.save(userMapper.toEntity(addUserDTO)));
     }
 
     @Override
