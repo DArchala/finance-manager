0c78fcfc6917e8cf76f4eb482e04f0eaf291161a
darch <damianarchala@gmail.com>
2024-04-01 16:04:55 +0200

    Use CrudRepository instead of JpaRepository

--- src/main/java/pl/archala/repository/BalancesRepository.java
+++ src/main/java/pl/archala/repository/BalancesRepository.java
@@ -1,13 +1,13 @@
 package pl.archala.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.archala.entity.Balance;
 
 @Repository
-public interface BalancesRepository extends JpaRepository<Balance, String> {
+public interface BalancesRepository extends CrudRepository<Balance, String> {
 
     @Modifying
     @Query("update Balance b set b.dailyTransactionsCount = 0")
--- src/main/java/pl/archala/repository/UsersRepository.java
+++ src/main/java/pl/archala/repository/UsersRepository.java
@@ -1,13 +1,13 @@
 package pl.archala.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.archala.entity.User;
 
 import java.util.Optional;
 
 @Repository
-public interface UsersRepository extends JpaRepository<User, Long> {
+public interface UsersRepository extends CrudRepository<User, Long> {
 
     Optional<User> findUserByUsername(String username);
     Optional<User> findUserByEmail(String email);
