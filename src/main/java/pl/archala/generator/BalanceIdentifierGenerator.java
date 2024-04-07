c63606491b602970887e143c2bf2941233b4fd3c
darch <damianarchala@gmail.com>
2024-04-07 23:05:51 +0200

    Remove duplicates from balance id generator

--- src/main/java/pl/archala/generator/BalanceIdentifierGenerator.java
+++ src/main/java/pl/archala/generator/BalanceIdentifierGenerator.java
@@ -6,19 +6,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.SecureRandom;
+import java.util.List;
 
 @Component
 public class BalanceIdentifierGenerator implements IdentifierGenerator {
 
     private final int length;
     private final SecureRandom random;
-    private final String characters;
+    private final List<Character> characters;
 
     public BalanceIdentifierGenerator(@Value("${balance.id.length}") int length,
                                       @Value("${balance.id.characters}") String characters) {
         this.length = length;
         this.random = new SecureRandom();
-        this.characters = characters;
+        this.characters = characters.chars().mapToObj(c -> (char) c).distinct().toList();
     }
 
     /*
@@ -31,7 +32,7 @@ public class BalanceIdentifierGenerator implements IdentifierGenerator {
     public Object generate(SharedSessionContractImplementor session, Object object) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            sb.append(characters.charAt(random.nextInt(characters.length())));
+            sb.append(characters.get(random.nextInt(characters.size())));
         }
         return sb.toString();
     }
